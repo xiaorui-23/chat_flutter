4980192c6b542804078d8b430614eee78f85122c
xiaoRui <3399251596@qq.com>
2023-05-06 18:02:20 +0800

    对 ChatViewItem 参数传递进行大调整重构；增加视频展示及播放；对修改及内容进行文档说明及修改；修改版本号及版本日志；

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:chat_flutter/chat_flutter.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
   runApp(MyApp());
@@ -31,16 +32,22 @@ class _MyAppState extends State<MyApp> {
                 senderRight: false,
                 itemBody: "Hi",
             ),
-            const ChatViewItem(
+            ChatViewItem(
                 itemBody: "不显示头像",
-                isAvatarShow: false,
-                isOpenTextSelect: true
+                avatarModel: ChatViewItemAvatarModel(
+                    isAvatarShow: false,
+                ),
+                textTypeModel: ChatViewItemTextTypeModel(
+                    isOpenTextSelect: true
+                ),
             ),
             const ChatViewItem(
                 itemBodyType: ChatViewItemRecordBodyType.audio,
             ),
-            const ChatViewItem(
-                audioTimelength: 10,
+            ChatViewItem(
+                audioTypeModel: ChatViewItemAudioTypeModel(
+                    audioTimelength: 10,
+                ),
                 itemBodyType: ChatViewItemRecordBodyType.audio,
             ),
             const ChatViewItem(
@@ -51,31 +58,47 @@ class _MyAppState extends State<MyApp> {
                 customItem: Text('自定义消息主体'),
             ),
             ChatViewItem(
-                isOpenTextSelect: true,
+                textTypeModel: ChatViewItemTextTypeModel(
+                    isOpenTextSelect: true,
+                    createSelectableTextCallback:(focusNode) {
+                        _focusNode = focusNode;
+                    },
+                ),
                 itemBody: "我是长按文字会出现菜单的文字",
-                createSelectableTextCallback:(focusNode) {
-                    _focusNode = focusNode;
-                },
             ),
-            const ChatViewItem(
+            ChatViewItem(
                 senderRight: false,
-                avatarPath: "https://img1.baidu.com/it/u=3007048469,3759326707&fm=253&fmt=auto&app=120&f=JPEG?w=889&h=500",
+                avatarModel: ChatViewItemAvatarModel(
+                    avatarPath: "https://img1.baidu.com/it/u=3007048469,3759326707&fm=253&fmt=auto&app=120&f=JPEG?w=889&h=500",
+                ),
                 itemBodyRecordTime: '2023-12-13',
                 itemBody: "带记录时间",
             ),
             ChatViewItem(
                 itemBodyType: ChatViewItemRecordBodyType.image,
                 itemBody: "https://pic35.photophoto.cn/20150511/0034034892281415_b.jpg",
-                previewImageLongPressMenu: const ['保存'],
-                onPreviewImageTapMenu:(data, index, menuList) {
-                    debugPrint("$data $index $menuList");
-                },
-                // customPreviewImageCallback:(imagePath) {
-                //     print(imagePath);
-                // },
-                // customLongPress:(context) {
-                //     print('customLongPress');
-                // },
+                imageTypeModel: ChatViewItemImageTypeModel(
+                    previewImageLongPressMenu: const ['保存'],
+                    onPreviewImageTapMenu:(data, index, menuList) {
+                        debugPrint("$data $index $menuList");
+                    },
+                    // customPreviewImageCallback:(imagePath) {
+                    //     print(imagePath);
+                    // },
+                    // customLongPress:(context) {
+                    //     print('customLongPress');
+                    // },
+                ),
+            ),
+            const ChatViewItem(
+                itemBodyType: ChatViewItemRecordBodyType.video,
+                itemBody: "https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4",
+                // videoTypeModel: ChatViewItemVideoTypeModel(
+                //     videoLoadFailCallback: (error) {},
+                //     notPlayingWidget: const Text('not Playing Widget'),
+                //     playingFailWidget: const Text('playing Fail Widget'),
+                //     autoPlaying: true,
+                // ),
             ),
             const ChatViewItem(
                 senderRight: false,
@@ -87,13 +110,17 @@ class _MyAppState extends State<MyApp> {
                 itemBodyType: ChatViewItemRecordBodyType.image,
                 itemBody: "https://img1.baidu.com",
             ),
-            const ChatViewItem(
+            ChatViewItem(
                 senderRight: false,
-                avatarPath: "https://img1.baidu.com/it/u=3007048469,3759326707&fm=253&fmt=auto&app=120&f=JPEG?w=889&h=500",
+                avatarModel: ChatViewItemAvatarModel(
+                    avatarPath: "https://img1.baidu.com/it/u=3007048469,3759326707&fm=253&fmt=auto&app=120&f=JPEG?w=889&h=500",
+                ),
                 itemBody: "头像网络图片",
             ),
-            const ChatViewItem(
-                avatarPath: "assets/logo.png",
+            ChatViewItem(
+                avatarModel: ChatViewItemAvatarModel(
+                    avatarPath: "assets/logo.png",
+                ),    
                 itemBody: "头像本地图片",
             ),
             const ChatViewItem(
@@ -107,6 +134,13 @@ class _MyAppState extends State<MyApp> {
     @override
     Widget build(BuildContext context) {
         return MaterialApp(
+            locale: const Locale('zh'),
+            localizationsDelegates: const [
+                GlobalMaterialLocalizations.delegate,
+                GlobalWidgetsLocalizations.delegate,
+                GlobalCupertinoLocalizations.delegate
+            ],
+            supportedLocales: const [Locale('zh', 'CH')],
             home: Scaffold(
                 appBar: AppBar(
                     centerTitle: true,
